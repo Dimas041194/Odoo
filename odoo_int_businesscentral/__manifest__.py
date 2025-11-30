78fb5f3575f132bb6e98452178f60a9d55cf101c
Dimas041194 <110814844+Dimas041194@users.noreply.github.com>
2025-11-30 19:11:52 +0700

    odoo_integrasi_businesscentral365

---
+++ odoo_int_businesscentral/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'BC Purchase Invoice Integration',
+    'version': '1.0',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/vendor_bill_sync_views.xml',
+    ],
+    'installable': True,
+    
+    
+}
+
+#DAN-2025-ODOO-TO-BC-ICHITAN-PROJECT#
+#INTEGRATION-TOKENAUTOMATIC#
+#1-Mandays#
